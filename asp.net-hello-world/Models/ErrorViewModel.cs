f06d19f56f6753f6c1471e166f07b11a6e80b09b
sachinxtensible <sachin@xtensible.in>
2022-11-14 14:51:18 +0530

    hello world

---
+++ asp.net-hello-world/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace asp.net_hello_world.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
